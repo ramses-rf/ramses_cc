c578528ea68c453006a17cadf30f68c8720166ee
Egbert Broerse <dcc2@ebroerse.nl>
2025-07-28 21:10:40 +0200

    Mock gwy.send_command

--- tests/tests_old/test_services.py
+++ tests/tests_old/test_services.py
@@ -221,8 +221,8 @@ SERVICES = {
         # ) -> asyncio.Task[Packet]:
         # Requires imports + setting up ramses_rf.Gateway and Heat Device first?
         # Directly call: "ramses_rf.system.heat.SysMode.set_mode" ->
-        "ramses_tx.command.Command.set_system_mode",
-        # "ramses_rf.gateway.Gateway.async_send_cmd", produces different reply
+        # "ramses_tx.command.Command.set_system_mode",
+        "ramses_rf.gateway.Gateway.send_cmd",  # async_send_command produces different reply
         # instead of
         # "custom_components.ramses_cc.climate.RamsesController.async_set_system_mode",
         SCH_SET_SYSTEM_MODE,
